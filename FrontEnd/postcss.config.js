8ad1550ced598cf46cf66620f661137a56e697f6
Footprintarts <mallyac17@gmail.com>
2024-09-04 13:44:51 +0300

    Update build artifacts and configurations

--- FrontEnd/postcss.config.js
+++ FrontEnd/postcss.config.js
@@ -1,6 +1,7 @@
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+
+// Export the PostCSS configuration
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+  plugins: [tailwindcss(), autoprefixer()],
+};
--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import tailwindcss from "tailwindcss";
-import autoprefixer from "autoprefixer";
+import postcss from "./postcss.config.js";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), autoprefixer()],
+  plugins: [react()],
+  css: {
+    postcss,
+  },
 });
